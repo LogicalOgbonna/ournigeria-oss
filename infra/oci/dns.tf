772fd4fe4992b079473f1f1a199d95f1e1058a7c
Arinze Ogbonna <arinzeogbo@gmail.com>
2026-06-25 05:44:12 +0200

    feat(infra): cloudflare A records for api/ingest/socials + outputs

---
+++ infra/oci/dns.tf
@@ -0,0 +1,17 @@
+locals {
+  api_hosts = {
+    api     = "api.ournigeria.ng"
+    ingest  = "ingest.ournigeria.ng"
+    socials = "socials.ournigeria.ng"
+  }
+}
+
+resource "cloudflare_dns_record" "api_tier" {
+  for_each = local.api_hosts
+  zone_id  = var.cloudflare_zone_id
+  name     = each.value
+  type     = "A"
+  content  = oci_core_instance.prod.public_ip
+  ttl      = 60
+  proxied  = false
+}
